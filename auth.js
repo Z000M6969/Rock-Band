1eb1898a3d09d377d9a78333cb384a55c2ce6247
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-09-16 09:35:04 -0300

    auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-/// === CONFIGURAÇÃO SUPABASE ===
+// === CONFIGURAÇÃO SUPABASE ===
 const SUPABASE_URL = "https://vwbbzvwluvgllkueixqo.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YmJ6dndsdXZnbGxrdWVpeHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg4NzksImV4cCI6MjA3MzUxNDg3OX0.vap3Az_gUqwYJ1MxFHdFDAjBx51iI9ucbGYNVb8lBfY";
 
