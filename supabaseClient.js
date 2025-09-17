caf5004bdc2499c1943901fb962f1f16a8f474a2
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-09-17 12:40:09 -0300

    supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+// supabaseClient.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+// ⚠️ Substitua pela sua URL e anon key
+const SUPABASE_URL = "https://vwbbzvwluvgllkueixqo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YmJ6dndsdXZnbGxrdWVpeHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg4NzksImV4cCI6MjA3MzUxNDg3OX0.vap3Az_gUqwYJ1MxFHdFDAjBx51iI9ucbGYNVb8lBfY";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
