65a73344b1f8cbd799a91af630349b518e3dd12f
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-09-17 14:48:39 -0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,3 +5,4 @@ const SUPABASE_URL = "https://vwbbzvwluvgllkueixqo.supabase.co";
 const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
