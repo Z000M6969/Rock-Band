e0a0409f5e8944834ac069355f02dcb7e854690f
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-09-18 10:07:58 -0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,12 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// Substitua pela sua URL e anon key
 const SUPABASE_URL = "https://vwbbzvwluvgllkueixqo.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YmJ6dndsdXZnbGxrdWVpeHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg4NzksImV4cCI6MjA3MzUxNDg3OX0.vap3Az_gUqwYJ1MxFHdFDAjBx51iI9ucbGYNVb8lBfY";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,       // mantém a sessão salva
+    autoRefreshToken: true,     // renova token automaticamente
+    detectSessionInUrl: true,   // detecta sessão no callback de login
+  },
+});
