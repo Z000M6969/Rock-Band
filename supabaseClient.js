5372b0f12e996d2aa56d539e2a610c98bb42c896
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-09-18 16:07:38 -0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,12 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2"; // Substitua pela sua URL e anon key 
-const SUPABASE_URL = "https://vwbbzvwluvgllkueixqo.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YmJ6dndsdXZnbGxrdWVpeHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg4NzksImV4cCI6MjA3MzUxNDg3OX0.vap3Az_gUqwYJ1MxFHdFDAjBx51iI9ucbGYNVb8lBfY"; 
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY), {
+const SUPABASE_URL = "https://vwbbzvwluvgllkueixqo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YmJ6dndsdXZnbGxrdWVpeHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg4NzksImV4cCI6MjA3MzUxNDg3OX0.vap3Az_gUqwYJ1MxFHdFDAjBx51iI9ucbGYNVb8lBfY";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true
   }
 });
+
