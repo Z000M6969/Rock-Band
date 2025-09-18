8b709880f7cf3c5ec75988b74af834dade9c7c8d
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-09-18 13:15:41 -0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 // supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://SEU-PROJETO.supabase.co";
-const supabaseKey = "PUBLIC-ANON-KEY";
+const supabaseUrl = "https://vwbbzvwluvgllkueixqo.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YmJ6dndsdXZnbGxrdWVpeHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mzg4NzksImV4cCI6MjA3MzUxNDg3OX0.vap3Az_gUqwYJ1MxFHdFDAjBx51iI9ucbGYNVb8lBfY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
